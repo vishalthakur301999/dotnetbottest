aaee61e732520ef0781233ce3d9ae2256631e913
Vishal Thakur <vishalthakur301999@gmail.com>
2019-05-21 12:47:32 +0530

    init

---
+++ Controllers/ProductionController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+using System.IO;
+using Newtonsoft.Json.Linq;
+using DialogflowFullfillmentTemplate.Models;
+
+namespace DialogflowFullfillmentTemplate.Controllers
+{
+    [Route("dialogflow/[controller]")]
+    public class ProductionController : Controller
+    {
+        // POST dialogflow/production
+        /// <summary>
+        /// This method receives the post request from dialog flow and binds the json to the value object.
+        /// You then use the JsonResponse class to build a reponse to return for dialog flow.
+        /// </summary>
+        /// <returns>The Json of the JsonResponse object for dialog to use</returns>
+        /// <param name="value">Value.</param>
+        [HttpPost]
+        public JsonResult Post([FromBody]JsonRequest value)
+        {
+            JsonResponse response = new JsonResponse();
+            response.Speech = "Here is some sample speech returned from the fullfillment service";
+            response.DisplayText = "Here is some sample display text returned from the fullfillment service";
+            response.Source = "sample fullfillment api";
+            return Json(response);
+        }
+      
+    }
+}
---
+++ Controllers/TestingController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using DialogflowFullfillmentTemplate.Models;
+using DialogflowFullfillmentTemplate.Models2;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace DialogflowFullfillmentTemplate.Controllers
+{
+    /// <summary>
+    /// Testing controller. This controller is used to test new methods while not interfereing with the production controller
+    /// </summary>
+    [Route("dialogflow/[controller]")]
+    public class TestingController : Controller
+    {
+        // POST dialogflow/testing
+        /// <summary>
+        /// This method receives the post request from dialog flow and binds the json to the value object.
+        /// You then use the JsonResponse class to build a reponse to return for dialog flow.
+        /// </summary>
+        /// <returns>The Json of the JsonResponse object for dialog to use</returns>
+        /// <param name="value">Value.</param>
+        [HttpPost]
+        public JsonResult Post([FromBody]JsonRequestV2 value)
+        {
+            JsonResponseV2 response = new JsonResponseV2();
+            response.FulfillmentText = "Hi, here is a response from the server";
+            response.Source = "sample fullfillment api";
+            return Json(response);
+        }
+    }
+}
+
